cf8a074abd22886bd67b354625d312dfbef8e52e
andris95 <szaniszlo.andris95@gmail.com>
2017-06-15 16:37:43 +0300

    init

--- app/src/main/java/com/boost/daggerquickstart/MainActivity.java
+++ app/src/main/java/com/boost/daggerquickstart/MainActivity.java
@@ -2,12 +2,33 @@ package com.boost.daggerquickstart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @BindView(R.id.edt_data)
+    EditText edtData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_save)
+    public void onClickSave() {
+        String data = edtData.getText().toString();
+        makeToast(data);
+    }
+
+    private void makeToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
